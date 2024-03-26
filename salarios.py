5bae908c63687bcb430be3c47b0f2c6285ea6836
Tatiana Ribeiro <tatiroliveira08@gmail.com>
2024-03-26 18:01:49 -0300

    ...

--- salarios.py
+++ salarios.py
@@ -1,34 +1,88 @@
 import pandas as pd
-import numpy as np 
-import datetime 
-# print(pd.__version__) 
-
+import numpy as np  
+import time 
+from datetime import datetime 
 
 def main():
     
-    salaries_data = pd.read_csv("salarios/salaries.csv")
-    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    #lendo apenas a coluna desejada do arquivo 
+    df = pd.read_csv("/home/tati/ed2/salarios/salaries.csv", usecols=['salary'])
+    salaries_data = pd.DataFrame(df)
+    pd.set_option("display.max_rows", 3000, "display.max_columns", None)
+    print("\n------DESORDENADO------\n")
+    print(salaries_data)
 
     # print(salaries_data.to_string())
-    
-    work_year = salaries_data.iloc[:,[0]]
-    experience_level = salaries_data.iloc[:,[1]]
-    employment_type = salaries_data.iloc[:,[2]]
-    job_title = salaries_data.iloc[:,[3]]
-    salary = salaries_data.iloc[:,[4]]
-    salary_currency = salaries_data.iloc[:,[5]]
-    salary_in_usd = salaries_data.iloc[:,[6]]
-    employee_residence = salaries_data.iloc[:,[7]]
-    remote_ratio = salaries_data.iloc[:,[8]]
-    company_location = salaries_data.iloc[:,[9]]
-    company_size = salaries_data.iloc[:,[10]]
-    
-    # print(salaries_data.iloc[:,[2]])
 
-    #ordenar crescente 
-    ordem_crescente = salaries_data.sort_values(by=4, axis=1, ascending=True)
-    print(ordem_crescente)
+    # salary = salaries_data.iloc[:,[4]]
 
+    start_time = time.time()
+    # start_time = datetime.now()
+    #ordenar crescente quick_sort
+    ordem_crescenteQS = salaries_data.sort_values(by='salary', ascending=True, kind="quicksort")
+    cresQStime = time.time() - start_time
+    print("\n-----CRESCENTE QS------\n")
+    print(ordem_crescenteQS)
+    
+
+    #ordenar decrescente quick_sort 
+    ordem_decrescenteQS = salaries_data.sort_values(by='salary', ascending=False, kind="quicksort")
+    decresQStime = time.time() - start_time
+    print("\n------ DECRESCENTE QS-------\n")
+    print(ordem_decrescenteQS)
+    
+
+    #ordem inversa QS
+    ordem_inversaQS = ordem_crescenteQS.sort_values(by='salary', ascending=False, kind="quicksort")
+    inversaQStime = time.time() - start_time
+    print("\n ----ORDEM INVERSA QS------\n")
+    print(ordem_inversaQS)
+
+    #ordenar crescente merge_sort
+    ordem_crescenteMS = salaries_data.sort_values(by='salary', ascending=True, kind="mergesort")
+    print("\n------ CRESCENTE MS-------\n")
+    print(ordem_crescenteMS)
+    cresMStime = time.time() - start_time
+
+    # ordenar decrescente merge_sort
+    ordem_decrescenteMS = salaries_data.sort_values(by='salary', ascending=False, kind="mergesort")
+    print("\n------ DECRESCENTE MS-------\n")
+    print(ordem_decrescenteMS)
+    decresMStime = time.time() - start_time
+
+    # ordem inversa merge sort
+    ordem_inversaMS = ordem_crescenteMS.sort_values(by='salary', ascending=False, kind="mergesort")
+    inversaMStime = time.time() - start_time 
+    print("\n ----ORDEM INVERSA MS------\n")
+    print(ordem_inversaMS)
+
+    #ordenar crescente heap_sort
+    ordem_crescenteHS = salaries_data.sort_values(by='salary', ascending=True, kind="heapsort")
+    print("\n------ CRESCENTE HS-------\n")
+    print(ordem_crescenteHS)
+    crescHStime = time.time() - start_time
+
+    #ordenar decrescente heap_sort
+    ordem_decrescenteHS = salaries_data.sort_values(by='salary', ascending=False, kind="heapsort")
+    print("\n------ DECRESCENTE HS-------\n")
+    print(ordem_decrescenteHS)
+    decresHStime = time.time() - start_time
+
+    # ordem inversa heap_sort
+    ordem_inversaHS = ordem_crescenteHS.sort_values(by='salary', ascending=False, kind="heapsort")
+    inversaHStime = time.time() - start_time 
+    print("\n ----ORDEM INVERSA HS------\n")
+    print(ordem_inversaHS)
+
+    print(f"Quick sort tempo para ordem crescente: {cresQStime}")
+    print(f"Quick Sort tempo para ordem decrescente: {decresQStime}")
+    print(f"Quick Sort tempo para ordem inversa (cresc -> decres): {inversaQStime}")
+    print(f"Merge Sort tempo para ordem crescente: {cresMStime}")
+    print(f"Merge Sort tempo para ordem decrescente: {decresMStime}")
+    print(f"Merge Sort tempo para ordem inversa (cresc -> decres): {inversaMStime}")
+    print(f"Heap Sort tempo para ordem crescente: {crescHStime}")
+    print(f"Heap Sort tempo para ordem decrescente: {decresHStime}")
+    print(f"Heap Sort tempo para ordem inversa (cresc -> decres): {inversaHStime}")
 
 if __name__ == '__main__':
     main()
