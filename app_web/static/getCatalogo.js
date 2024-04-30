ca28074108f27c4a34271cc0368d939de2cfcc42
Tatiana Ribeiro <tatiroliveira08@gmail.com>
2024-04-30 00:01:39 -0300

    add livro

--- app_web/static/getCatalogo.js
+++ app_web/static/getCatalogo.js
@@ -70,7 +70,5 @@ async function mostrarCatalogo(dicionario)
         painelEstoque.innerHTML = "Estoque: " + estoqueLivro;
         quadradoGrande.appendChild(painelEstoque);
 
-
-        // console.log(value)
     });
 }
